39ef50ab2fb39ecc84cb18a473de7e5f01fbe7df
Kellen Baker <mkellenbaker@gmail.com>
2019-07-18 10:13:24 -0500

    componentize Header

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+//components
+import Header from "../Header/Header";
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <h1>Pet Hotel</h1>
+      <Header />
     </div>
   );
+  }
 }
 
 export default App;
