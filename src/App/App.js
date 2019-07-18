e40e8d082d0ea80032da4ada9ceb4b6b21047963
kibretg <kibretrg@gmail.com>
2019-07-18 11:20:27 -0500

    owners page created

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import './App.css';
+import '../OwnersView/OwnersView'
 
 //components
 import Header from "../Header/Header";
+import OwnersView from '../OwnersView/OwnersView';
 
 class App extends Component {
   render() {
   return (
     <div className="App">
       <Header />
+      <OwnersView/>
     </div>
   );
   }
