0756c18f67804e39f90307af57ca4fae97fbc350
Kellen Baker <mkellenbaker@gmail.com>
2019-07-19 10:29:48 -0500

    resolved conflicts 2

--- src/App/App.js
+++ src/App/App.js
@@ -4,22 +4,16 @@ import '../OwnersView/OwnersView'
 
 //components
 import Header from "../Header/Header";
-<<<<<<< HEAD
 import OwnersView from '../OwnersView/OwnersView';
-=======
 import Dashboard from "../Dashboard/Dashboard";
->>>>>>> master
 
 class App extends Component {
   render() {
   return (
     <div className="App">
       <Header />
-<<<<<<< HEAD
       <OwnersView/>
-=======
       <Dashboard />
->>>>>>> master
     </div>
   );
   }
